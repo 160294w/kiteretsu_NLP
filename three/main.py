d245c8d85b97bad150786e9cc4969f9010d9d261
160294w <160294w@gmail.com>
2022-03-08 00:01:15 +0900

    Add: メロスの激怒具合を調べてみた

--- three/main.py
+++ three/main.py
@@ -6,16 +6,37 @@ for i, row in enumerate(reader):
     name = row[0]
     result = row[1]
     np_dic[name] = result
-    if i % 1000 == 0: print(i)
 print("OK")
 
-print(np_dic["激怒"])
-print(np_dic["苦情"])
-print(np_dic["糞"])
-print(np_dic["喜び"])
-print(np_dic["勝利"])
-print(np_dic["上品"])
-print(np_dic["商品"])
-print(np_dic["奔走"])
-print(np_dic["時間"])
+# Janomeのロード
+from janome.tokenizer import Tokenizer
 
+# Tokenizerインスタンスの生成 
+tokenizer = Tokenizer()
+
+# 入力した文字列に対して、
+# ポジティブ単語数、ネガティブ単語数、全単語数、の3つを返す
+def np_rate(input_str):
+  pos_count  = 0
+  neg_count  = 0
+  word_count = 0
+  
+  tokens = tokenizer.tokenize(input_str)
+  for token in tokens:
+    base_form = token.base_form # 原形/基本形
+    # ネガポジ辞書に存在するか確認して対応する方を1増やす
+    if base_form in np_dic:
+      # 単語を辞書のキーとして、そのバリューが p か n か確認する
+      if np_dic[base_form] == "p" :
+        pos_count += 1
+        # どんな言葉がポジ判定されてるか確認用（あとでコメントアウト）
+        print("POS:" + base_form) 
+      if np_dic[base_form] == "n" :
+        neg_count += 1
+        # どんな言葉がネガ判定されてるか確認用（あとでコメントアウト）
+        print("NEG:" + base_form) 
+    # 存在しようがしまいが、単語数を1増やす
+    word_count += 1
+  return pos_count, neg_count, word_count
+
+print(np_rate("メロスは激怒した。必ず、かの邪智暴虐の王を除かなければならぬと決意した。"))
